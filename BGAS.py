82b71bddbe85a567e45d8aa6f46b3b6a961b2a6a
Lojcs <lojcsgit@gmail.com>
2022-04-14 00:01:48 +0300

    fixed administrator.py again

--- BGAS.py
+++ BGAS.py
@@ -13,7 +13,7 @@
 # TODO: Include dependecies
 # TODO: Remove pssuspend64.exe dependecy requirement
 
-# Version 2.0.2
+# Version 2.0.3
 
 try:
 	import win32gui, pywinauto
--- administrator.py
+++ administrator.py
@@ -2,4 +2,4 @@
 # You should have received a copy of the GNU General Public License along with this program.  If not, see https://www.gnu.org/licenses.
 
 import ctypes, os, sys
-ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, f"{os.getcwd()}\\BGAS.pyw", None, 1)
+ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, f"{os.getcwd()}\\BGAS.py", None, 1)
