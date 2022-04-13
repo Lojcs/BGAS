2154243fb59ba10fed1a84628a720af0217f8e8d
Lojcs <lojcsgit@gmail.com>
2022-04-13 18:49:08 +0300

    Licence header

--- elevator.py
+++ elevator.py
@@ -1,2 +1,5 @@
+# BGAS Copyright (C) 2022  Lojcs
+# You should have received a copy of the GNU General Public License along with this program.  If not, see https://www.gnu.org/licenses.
+
 import ctypes, os, sys
 ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, f"{os.getcwd()}\\BGAS.py", None, 1)
